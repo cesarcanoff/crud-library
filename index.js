326f4b37382f210795f16dad3fce1f3016376acd
cesarcanoff <canoff.cesar@gmail.com>
2022-09-20 00:31:25 -0400

    Create and List Books from MySQL Database

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
 const express = require("express");
-
 const app = express();
 const PORT = 3333;
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "canoff",
+  password: "password",
+});
+
+connection.connect((error) => {
+  if (error) throw error;
+  console.log("Connected!");
+});
 
 app.use(express.json());
 
 app.get("/", (request, response) => {
-  response.json({});
+  response.json({ Status: "OK" });
+});
+
+app.post("/book", (request, response) => {
+  let data = request.body;
+
+  let sql = `INSERT INTO library.book (isbn, title, author) VALUES ('${data.isbn}', '${data.title}', '${data.author}');`;
+  connection.query(sql, (error, result) => {
+    if (error) throw error;
+    response.statusCode = 201;
+    response.json({"Success": true});
+  });
 });
 
 app.get("/books", (request, response) => {
-  response.json({});
+  let sql = "SELECT isbn, title, author FROM library.book;";
+  connection.query(sql, (error, result) => {
+    if (error) throw error;
+    response.json(result);
+  });
 });
 
-app.get("/authors", (request, response) => {
-  response.json({});
-})
+app.get("/book/:isbn", (request, response) => {
+  const isbn = request.params.isbn;
+  const query = `SELECT isbn, title, author FROM library.book WHERE isbn = '${isbn}';`;
+  connection.query(query, (error, result) => {
+    if (error) throw error;
+    response.json(result);
+  });
+});
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log("Server is running at localhost:3333");
+});
