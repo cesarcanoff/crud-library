ead6643d530345e4cb5568b961a3c02434e68365
cesarcanoff <canoff.cesar@gmail.com>
2022-09-28 02:00:21 -0400

    Code: Add Try/Catch

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const server = express();
 const PORT = 3333;
-
-server.use(express.json());
-
 const { connection } = require("./src/database/database");
 
+server.use(express.json());
 server.get("/", (request, response) => response.json({ Status: "OK" }));
 
 /*
@@ -16,14 +14,40 @@ server.get("/", (request, response) => response.json({ Status: "OK" }));
   	"pages": 431,
   	"category": "Tech",
   	"year": 2008,
-  	"language": "English"
+  	"language": "English",
+    cover: "https://images-na.ssl-images-amazon.com/images/I/41xShlnTZTL.jpg"
   }
 */
 
-server.post("/book", (request, response) => {
-  let data = request.body;
+server.get("/books", (request, response) => {
   try {
-    let sql = `INSERT INTO library.book (isbn, title, author, pages, category, year, language) VALUES ('${data.isbn}', '${data.title}', '${data.author}', '${data.pages}', '${data.category}', '${data.year}', '${data.language}');`;
+    const sql = "SELECT title FROM Library.Books;";
+    connection.query(sql, (error, result) => {
+      if (error) throw error;
+      response.json(result);
+    });
+  } catch (error) {
+    throw error;
+  }
+});
+
+server.get("/book/:isbn", (request, response) => {
+  try {
+    const isbn = request.params.isbn;
+    const query = `SELECT isbn, title, author FROM library.book WHERE isbn = '${isbn}';`;
+    connection.query(query, (error, result) => {
+      if (error) throw error;
+      response.json(result);
+    });
+  } catch (error) {
+    throw error;
+  }
+});
+
+server.post("/book", (request, response) => {
+  try {
+    let data = request.body;
+    let sql = `INSERT INTO Library.Books (isbn, title, author, pages, category, year, language, cover) VALUES ('${data.isbn}', '${data.title}', '${data.author}', '${data.pages}', '${data.category}', '${data.year}', '${data.language}, '${data.cover}');`;
     connection.query(sql, (error, result) => {
       response.status(201).json({ Success: true });
     });
@@ -32,56 +56,42 @@ server.post("/book", (request, response) => {
   }
 });
 
-server.delete("/book/:id", (request, response) => {
-  const id_book = request.params.id;
-  const sql = `DELETE FROM library.book WHERE id = '${id_book}';`;
-  connection.query(sql, (error, result) => {
-    if (error) throw error;
-    response.json(result);
-  });
-
-})
-
-
 server.put("/book/:isbn", (request, response) => {
-  const isbn = request.params.isbn;
-  let data = request.body;
+  try {
+    const isbn = request.params.isbn;
+    let data = request.body;
+  
+    let sql = `UPDATE library.book SET `;
+  
+    if (data.title) {
+      sql += `title = '${data.title}', `;
+    }
+  
+    if (data.author) {
+      sql += `author = '${data.author}' `;
+    }
+  
+    sql += `WHERE isbn = '${isbn}';`;
 
-  let sql = `UPDATE library.book SET `;
-
-  if (data.title) {
-    sql += `title = '${data.title}', `;
+    connection.query(sql, (error, result) => {
+      if (error) throw error;
+      response.json(result);
+    });
+  } catch (error) {
+    throw error;
   }
-
-  if (data.author) {
-    sql += `author = '${data.author}' `;
-  }
-
-  sql += `WHERE isbn = '${isbn}';`;
-  console.log(sql);
-
-  // sql = `UPDATE library.book SET title = '${data.title}' author = '${data.author}' WHERE isbn = '${isbn}';`;
-  connection.query(sql, (error, result) => {
-    if (error) throw error;
-    response.json(result);
-  });
 });
 
-server.get("/books", (request, response) => {
-  const sql = "SELECT * FROM library.book;";
-  connection.query(sql, (error, result) => {
-    if (error) throw error;
-    response.json(result);
-  });
-});
-
-server.get("/book/:isbn", (request, response) => {
-  const isbn = request.params.isbn;
-  const query = `SELECT isbn, title, author FROM library.book WHERE isbn = '${isbn}';`;
-  connection.query(query, (error, result) => {
-    if (error) throw error;
-    response.json(result);
-  });
+server.delete("/book/:id", (request, response) => {
+  try {
+    const id_book = request.params.id;
+    const sql = `DELETE FROM library.book WHERE id = '${id_book}';`;
+    connection.query(sql, (error, result) => {
+      response.json(result);
+    });
+  } catch (error) {
+    throw error;
+  }
 });
 
 server.listen(PORT);
