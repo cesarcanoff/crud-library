135ed757660e4719c1003b94ea29abdb10c1a49b
cesarcanoff <canoff.cesar@gmail.com>
2022-09-28 01:24:09 -0400

    Code: CRUD System Complete

--- index.js
+++ index.js
@@ -4,34 +4,57 @@ const PORT = 3333;
 
 server.use(express.json());
 
-const { connection } = require('./src/database/database');
+const { connection } = require("./src/database/database");
 
-server.get("/", (request, response) => {
-  response.json({"Status": "OK"})
-});
+server.get("/", (request, response) => response.json({ Status: "OK" }));
+
+/*
+  {
+	  "isbn": "9780132350884",
+  	"title": "Clean Code: A Handbook of Agile Software Craftsmanship",
+  	"author": "Robert C. Martin",
+  	"pages": 431,
+  	"category": "Tech",
+  	"year": 2008,
+  	"language": "English"
+  }
+*/
 
 server.post("/book", (request, response) => {
   let data = request.body;
-
-  let sql = `INSERT INTO library.book (isbn, title, author) VALUES ('${data.isbn}', '${data.title}', '${data.author}');`;
-  connection.query(sql, (error, result) => {
-    if (error) throw error;
-    response.status(201).json({"Success": true});
-  });
+  try {
+    let sql = `INSERT INTO library.book (isbn, title, author, pages, category, year, language) VALUES ('${data.isbn}', '${data.title}', '${data.author}', '${data.pages}', '${data.category}', '${data.year}', '${data.language}');`;
+    connection.query(sql, (error, result) => {
+      response.status(201).json({ Success: true });
+    });
+  } catch (err) {
+    throw err;
+  }
 });
 
-server.put('/book/:isbn', (request, response) => {
+server.delete("/book/:id", (request, response) => {
+  const id_book = request.params.id;
+  const sql = `DELETE FROM library.book WHERE id = '${id_book}';`;
+  connection.query(sql, (error, result) => {
+    if (error) throw error;
+    response.json(result);
+  });
+
+})
+
+
+server.put("/book/:isbn", (request, response) => {
   const isbn = request.params.isbn;
   let data = request.body;
 
   let sql = `UPDATE library.book SET `;
 
   if (data.title) {
-      sql += `title = '${data.title}', `;
+    sql += `title = '${data.title}', `;
   }
 
   if (data.author) {
-      sql += `author = '${data.author}' `;
+    sql += `author = '${data.author}' `;
   }
 
   sql += `WHERE isbn = '${isbn}';`;
@@ -42,20 +65,10 @@ server.put('/book/:isbn', (request, response) => {
     if (error) throw error;
     response.json(result);
   });
-    
 });
 
-
-
-
-
-
-
-
-
-
 server.get("/books", (request, response) => {
-  const sql = "SELECT isbn, title, author FROM library.book;";
+  const sql = "SELECT * FROM library.book;";
   connection.query(sql, (error, result) => {
     if (error) throw error;
     response.json(result);
